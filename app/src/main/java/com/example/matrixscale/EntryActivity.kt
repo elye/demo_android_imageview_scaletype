fbea69b9088918fc44565e66d1d42c2aab4170f2
elye <elye.project@gmail.com>
2020-09-18 19:52:14 +1000

    Add match wrap

--- app/src/main/java/com/example/matrixscale/EntryActivity.kt
+++ app/src/main/java/com/example/matrixscale/EntryActivity.kt
@@ -4,35 +4,56 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ImageView.ScaleType
+import android.widget.LinearLayout
+import android.widget.LinearLayout.LayoutParams.*
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout.*
 import com.example.matrixscale.MainActivity.Companion.IMAGE_TYPE_KEY
 import com.example.matrixscale.MainActivity.Companion.SCALE_TYPE_KEY
+import com.example.matrixscale.MainActivity.Companion.SIZE_TYPE_KEY
 import kotlinx.android.synthetic.main.activity_entry.*
 
 
-class EntryActivity: AppCompatActivity() {
+class EntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry)
 
         image_selector.adapter = ArrayAdapter(
-            this, android.R.layout.simple_spinner_item, ImagesTypeEnum.values())
+            this, android.R.layout.simple_spinner_item, ImagesTypeEnum.values()
+        )
 
         scale_selector.adapter = ArrayAdapter(
-            this, android.R.layout.simple_spinner_item, ScaleTypeEnum.values())
+            this, android.R.layout.simple_spinner_item, ScaleTypeEnum.values()
+        )
+
+        size_selector.adapter = ArrayAdapter(
+            this, android.R.layout.simple_spinner_item, SizeTypeEnum.values()
+        )
 
         button_show.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java).apply {
-                putExtra(IMAGE_TYPE_KEY, ImagesTypeEnum.getEnum(image_selector.selectedItem.toString()))
-                putExtra(SCALE_TYPE_KEY, ScaleTypeEnum.getEnum(scale_selector.selectedItem.toString()))
+                putExtra(
+                    IMAGE_TYPE_KEY,
+                    ImagesTypeEnum.getEnum(image_selector.selectedItem.toString())
+                )
+                putExtra(
+                    SCALE_TYPE_KEY,
+                    ScaleTypeEnum.getEnum(scale_selector.selectedItem.toString())
+                )
+                putExtra(
+                    SIZE_TYPE_KEY,
+                    SizeTypeEnum.getEnum(size_selector.selectedItem.toString())
+                )
             }
             startActivity(intent)
         }
     }
 }
 
-enum class ImagesTypeEnum(val descriptor: String, @DrawableRes val imageId : Int) {
+enum class ImagesTypeEnum(val descriptor: String, @DrawableRes val imageId: Int) {
     BIGGER("Bigger Image - Fall", R.drawable.fall),
     SMALLER("Smaller Image - Lion", R.drawable.lion),
     TALLER("Taller Image - Tree", R.drawable.tree),
@@ -65,6 +86,30 @@ enum class ScaleTypeEnum(val descriptor: String, val scaleType: ScaleType) {
 
     companion object {
         fun getEnum(value: String): ScaleTypeEnum {
+            return values().first { it.descriptor == value }
+        }
+    }
+}
+
+enum class SizeTypeEnum(
+    val descriptor: String,
+    val layoutParams: Pair<Int, Int>
+) {
+    MATCH_PARENT("All Match Parent",
+        Pair(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)),
+    WRAP_CONTENT("All Wrap Content",
+        Pair(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)),
+    MATCH_PARENT_WRAP_CONTENT("Height Match - Width Wrap",
+        Pair(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)),
+    WRAP_CONTENT_MATCH_PARENT("Height Wrap - Width Match",
+        Pair(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
+
+    override fun toString(): String {
+        return descriptor
+    }
+
+    companion object {
+        fun getEnum(value: String): SizeTypeEnum {
             return values().first { it.descriptor == value }
         }
     }
--- app/src/main/java/com/example/matrixscale/MainActivity.kt
+++ app/src/main/java/com/example/matrixscale/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.matrixscale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.withSign
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val IMAGE_TYPE_KEY = "IMAGETYPE"
         const val SCALE_TYPE_KEY = "SCALETYPE"
+        const val SIZE_TYPE_KEY = "SIZETYPE"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         image_picture.apply {
+            layoutParams.height = (intent.getSerializableExtra(SIZE_TYPE_KEY) as SizeTypeEnum).layoutParams.first
+            layoutParams.width = (intent.getSerializableExtra(SIZE_TYPE_KEY) as SizeTypeEnum).layoutParams.second
             setImageResource((intent.getSerializableExtra(IMAGE_TYPE_KEY) as ImagesTypeEnum).imageId)
             scaleType = (intent.getSerializableExtra(SCALE_TYPE_KEY) as ScaleTypeEnum).scaleType
         }
